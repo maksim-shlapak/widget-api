813493573f5332a725b4ac760a9c782eada0efea
Genadi Sokolov <genadi@annoto.net>
2018-05-10 16:15:03 +0300

    added timeline focusin and focusout ux events

--- lib/ux-event.d.ts
+++ lib/ux-event.d.ts
@@ -1,4 +1,4 @@
 export interface AnnotoUxEvent {
-    name: 'widget:hide' | 'widget:show' | 'timeline:hide' | 'timeline:show';
+    name: 'widget:hide' | 'widget:show' | 'timeline:hide' | 'timeline:show' | 'timeline:focusin' | 'timeline:focusout';
     data?: any;
 }
