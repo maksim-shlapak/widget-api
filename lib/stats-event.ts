7a18c6d33894e4d8945cf836c8667f33460bc76f
Maksim Shlapak <maksim.shlapak@annoto.net>
2021-06-03 15:51:46 +0300

    feat: added StatsEvent and VideoBenchmarkEvent types

---
+++ lib/stats-event.ts
@@ -0,0 +1,40 @@
+/**
+ * @packageDocumentation
+ * @module annoto
+ */
+
+export enum StatsType {
+    firstPlay = 1,
+    commentTimetagTap,
+    showReplyTap,
+    noteTimetagTap,
+    vote,
+    threadSort,
+    threadFollow,
+    commentKukuTap,
+    commentTap,
+    noteKukuTap,
+    noteTap,
+    timelineUgcTap,
+    syncTap,
+    comment,
+    reply,
+    note,
+    commentEdit,
+    commentDelete,
+    noteEdit,
+    noteDelete,
+    groupFollow,
+}
+
+export interface IAnnotoStatsEvent {
+    type: StatsType;
+    ugc_id?: number;
+    group_id: string;
+    visitor_id: string;
+    session_id: string;
+    ps?: number;
+    id?: string;
+    timestamp?: string;
+    media_src: string;
+}
